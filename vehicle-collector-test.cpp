55730d5c2136e3352229aeacaa2d7eff75db9f52
dileepd53 <68851145+dileepd53@users.noreply.github.com>
2020-08-03 19:06:57 +0530

    updated

--- vehicle-collector-test.cpp
+++ vehicle-collector-test.cpp
@@ -5,9 +5,9 @@
 TEST(VehicleTest, when_vehicle_reports_a_measuerement_it_is_fetched_and_stored){
  
  telematics vehicle_input_param  = {231, MOTOR_TEMP, 30};
- inventory vehicle_output_param = vehicleParamTester(vehicle_input_param); 
+ inventory *vehicle_output_param = vehicleParamTester(&vehicle_input_param); 
  
- ASSERT_EQ(vehicle_output_param.motor_temp, 30); 
+ ASSERT_EQ(vehicle_output_param->motor_temp, 30); 
  
 }
 
